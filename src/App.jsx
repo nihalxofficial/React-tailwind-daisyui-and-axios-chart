ceff87363e4782352508db80257689b0b6ca800b
Md. Nihal Uddin Bijoy <mdnihaluddinbijoy@gmail.com>
2026-03-17 20:22:06 +0600

    Line Cart Added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import MyNav from './components/MyNav/MyNav'
 import Navbar from './components/Navbar/Navbar'
 import Pricing from './components/Pricing/Pricing'
+import ResultChart from './components/ResultChart/ResultChart'
 
 const pricingPromise = fetch("./PricingData.json").then(res => res.json())
 
@@ -15,10 +16,15 @@ function App() {
         <MyNav></MyNav>
       </header>
 
-      <main className="my-20">
+      <main className="space-y-10 mt-20">
+        <ResultChart></ResultChart>
+
+        
         <Suspense fallback={<span className="loading loading-spinner loading-xl"></span>}>
           <Pricing pricingPromise={pricingPromise}></Pricing>
         </Suspense>
+
+        
 
       </main>
 
